3cf9acfeddd626f4c45b453085ad0736a6519142
Mayllon Veras <mayllonveras@gmail.com>
2019-06-04 10:44:38 -0300

    Primeira classe de domínio

---
+++ src/main/java/com/ifpi/ted2019/resources/CategoriaResource.java
@@ -0,0 +1,25 @@
+package com.ifpi.ted2019.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ifpi.ted2019.domain.Categoria;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Categoria> listar() {
+		List<Categoria> categorias = new ArrayList<Categoria>();
+		categorias.add(new Categoria(1, "Informática"));
+		categorias.add(new Categoria(2, "Escritório"));
+		return categorias;
+		
+	}
+
+}
