c4b9e4fb4758d878ad067f0e71f9645fe305be27
Mayllon Veras <mayllonveras@gmail.com>
2019-06-16 23:53:47 -0300

    Repository e Service para a classe Categoria

---
+++ src/main/java/com/ifpi/ted2019/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.ifpi.ted2019.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ifpi.ted2019.domain.Categoria;
+import com.ifpi.ted2019.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repository;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> object = repository.findById(id);
+		return object.orElse(null);
+	}
+}
