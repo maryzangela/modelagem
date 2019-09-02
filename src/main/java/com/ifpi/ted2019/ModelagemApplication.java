267335d88dc167e716209fe0d53296bfdb1e935f
Mayllon Veras <mayllonveras@gmail.com>
2019-09-02 06:22:40 -0300

    Classe Produto e relacionamento com Categoria

--- src/main/java/com/ifpi/ted2019/ModelagemApplication.java
+++ src/main/java/com/ifpi/ted2019/ModelagemApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ifpi.ted2019.domain.Categoria;
+import com.ifpi.ted2019.domain.Produto;
 import com.ifpi.ted2019.repositories.CategoriaRepository;
+import com.ifpi.ted2019.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class ModelagemApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemApplication.class, args);
@@ -23,10 +27,23 @@ public class ModelagemApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		Categoria categoria = new Categoria(null, "Informática");
+		Categoria categoria1 = new Categoria(null, "Informática");
 		Categoria categoria2 = new Categoria(null, "Escritório");
 		
-		categoriaRepository.saveAll(Arrays.asList(categoria, categoria2));
+		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+		
+		Produto produto1 = new Produto(null, "Computador", 2000.00);
+		Produto produto2 = new Produto(null, "Impressora", 8000.00);
+		Produto produto3 = new Produto(null, "Mouse", 80.00);
+		
+		categoria1.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3));
+		categoria2.getProdutos().addAll(Arrays.asList(produto2));
+		
+		produto1.getCategorias().addAll(Arrays.asList(categoria1));
+		produto2.getCategorias().addAll(Arrays.asList(categoria1, categoria2));
+		produto3.getCategorias().addAll(Arrays.asList(categoria1));
+		
+		produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
 		
 	}
 
