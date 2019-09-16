634e737a7c6c45e6c7b0c9971b4efa32851430a9
Maryzangela Cavalcante Lopes <2017118050236@ifpi.int>
2019-09-16 08:40:24 -0300

    cidade e estado implementados

--- src/main/java/com/ifpi/ted2019/ModelagemApplication.java
+++ src/main/java/com/ifpi/ted2019/ModelagemApplication.java
@@ -1,15 +1,17 @@
 package com.ifpi.ted2019;
-
+import java.lang.reflect.Array;
 import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import com.ifpi.ted2019.domain.Categoria;
+import com.ifpi.ted2019.domain.Cidade;
+import com.ifpi.ted2019.domain.Estado;
 import com.ifpi.ted2019.domain.Produto;
 import com.ifpi.ted2019.repositories.CategoriaRepository;
+import com.ifpi.ted2019.repositories.CidadeRepository;
+import com.ifpi.ted2019.repositories.EstadoRepository;
 import com.ifpi.ted2019.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +21,10 @@ public class ModelagemApplication implements CommandLineRunner{
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemApplication.class, args);
@@ -43,6 +49,19 @@ public class ModelagemApplication implements CommandLineRunner{
 		produto2.getCategorias().addAll(Arrays.asList(categoria1, categoria2));
 		produto3.getCategorias().addAll(Arrays.asList(categoria1));
 		
+		Estado estado1 = new Estado(null, "Piauí");
+		Estado estado2 = new Estado(null, "Ceará");
+		
+		Cidade cidade1= new Cidade(null, "Fortaleza", estado2);
+		Cidade cidade2= new Cidade(null, "Piripiri", estado1);
+		Cidade cidade3= new Cidade(null, "Parnaíba", estado1);
+		
+		estado1.getCidades().addAll(Arrays.asList(cidade2, cidade3));
+		estado2.getCidades().addAll(Arrays.asList(cidade1));
+		
+		estadoRepository.saveAll(Arrays.asList(estado1, estado2));
+		cidadeRepository.saveAll(Arrays.asList(cidade1, cidade2, cidade3));
+		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
 		produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
 		
 	}
---
+++ src/main/java/com/ifpi/ted2019/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.ifpi.ted2019.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.ifpi.ted2019.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
